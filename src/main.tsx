084cbadf3fc58fd669953691e0b290f19e075d54
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2026-03-02 15:55:32 -0600

    wrap main with auth0 provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,24 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience,
+      }}
+      cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
